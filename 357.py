212ecdddf5af6a3ca6cc545c2876645073524efd
Denis Gandzha <denis.gandzha@artezio.com>
2019-12-12 13:22:36 +0300

    added task

--- 357.py
+++ 357.py
@@ -11,19 +11,43 @@
 Если в массиве несколько максимальных элементов, то нужно вывести минимальный номер строки, в которой встречается такой элемент, а если в этой строке таких элементов несколько, то нужно вывести минимальный номер столбца. Не забудьте, что все строки и столбцы нумеруются с 0.
 
 """
+def isInt(value):
+	try:
+		int(value)
+		return True
+	except:
+		ValueError
+		return False
 
-inputData = input().split()
-n = int(inputData[0])
-m = int(inputData[1])
-arr = []
-arr2 = [[]]
-global maxElement 
-maxElement = 0
-for i in range(int(n)):
-	arr.append([ int(j) for j in input().split()])
-for i in range(0,int(n)):
-	for j in range(0,int(m)):
-		if(int(arr[i][j]) > maxElement):
-			maxElement = arr[i][j]
-			arr2[m] = (i,j)
-print(arr2)
+checkInput = input()
+if checkInput[0] != " " and checkInput[2] != " ":
+	inputData = checkInput.split()
+	#check array not empty
+	if inputData and len(inputData) == 2 and isInt(inputData[0]) and isInt(inputData[1]):
+		n = int(inputData[0])
+		m = int(inputData[1])
+		if n > 0 and m > 0:
+			startArray = []
+			arrayForIndex = list()
+			global maxElement 
+			maxElement = 0
+			#read data
+			for i in range(n):
+				startArray.append([ j for j in input().split()])	
+			#find max element
+			for i in range(0,int(n)):
+				if len(startArray[i]) < m:
+					exit(0)
+				for j in range(0,len(startArray[i])):
+					#check input data
+					if startArray[i][j] and isInt(startArray[i][j]):
+						#write index in maxElement array index
+						if int(startArray[i][j]) > int(maxElement):
+							maxElement = startArray[i][j]
+							arrayForIndex.insert(int(maxElement),[i,j]);
+					else:
+						exit(0)
+
+			print(maxElement)
+			print(arrayForIndex[len(arrayForIndex)-1][0],arrayForIndex[len(arrayForIndex)-1][1])
+
